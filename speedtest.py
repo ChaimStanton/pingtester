dcb34d34e17a9437d689e75c80bb4285cec21416
Chaim Stanton <chaimstanton1@gmail.com>
2020-05-06 21:18:26 +0100

    added log function

--- speedtest.py
+++ speedtest.py
@@ -1,7 +1,23 @@
 #! ./venv/bin/python3
 
 import urllib.request
+from datetime import datetime
+import logging
 
+logging.basicConfig(filename='speedtest.log', filemode='a', format='%(message)s')
+
+severityMessgDICT = {1 : "info",
+                     2 : "good",
+                     3 : "BAD"}
+
+def log(message, severity):
+    # 1 for informational 2 for good 3 for bad
+    text = "---" + severityMessgDICT[severity] + "--- " + "at --- " + timeSTR() + " --- " + message
+    logging.warning(text)
+
+def timeSTR():
+    a = datetime.now()
+    return a.strftime(r"%d/%m/%Y, %H:%M:%S")
 
 def am_I_connected(host="http://google.com"):
     try:
@@ -10,5 +26,12 @@ def am_I_connected(host="http://google.com"):
     except urllib.error.URLError:
         return False
 
+log("This is beginning of program format of date is day/month/year, hour, minute, second",1)
 
-print(am_I_connected())
+
+if am_I_connected() == True:
+    log("Connected to internet", 2)
+else:
+    log("Not connected to internet", 3)
+
+log("end of program", 1)
