7c1fe28074ee5a7156f3b75a2d80ec00452e8795
Chaim Stanton <chaimstanton1@gmail.com>
2020-05-13 02:11:22 +0100

    added stuff to footer

--- footer.php
+++ footer.php
@@ -6,39 +6,40 @@ echo '
 
 <footer>
 
-<div class="container-fluid bg-primary">
+<div class="container-fluid bg-light">
 	<div class="row table-bordered">
 		<div class="col-md-4">
 			<h2>
 				This project
 			</h2>
 			<p>
-				tTralala details copyright
+				This project was created using Ookla speed test from <a href="speedtest.net">speedtest.net</a>. This was created to test the speed of the internet connection every 5 minutes.
 			</p>
 			<p>
-				<a class="btn btn-secondary" href="#">View details »</a>
-			</p>
+				<a class="btn btn-primary" href="https://chaimstanton.github.io/speedtest/">See more on Github»</a>
+			</>
 		</div>
 		<div class="col-md-4">
 			<h2>
 				About me
 			</h2>
 			<p>
-				tralala
+				My name is Chaim Stanton, I am currently a computer science student, for more information see link below. 
 			</p>
 			<p>
-				<a class="btn btn-secondary" href="#">View details »</a>
+				<a class="btn btn-primary" href="www.linkedin.com/in/chaimstanton">View my Linkedin »</a>
 			</p>
 		</div>
 		<div class="col-md-4">
 			<h2>
-				contact
+				Additional information
 			</h2>
 			<p>
-				tralala
+				©Copyright Chaim Stanton 2020. <br>
+				This is open source and the source code is available on GitHub. 
 			</p>
 			<p>
-				<a class="btn btn-secondary" href="#">View details »</a>
+				<a class="btn btn-primary" href="https://github.com/ChaimStanton/speedtest">View GitHub Repo »</a>
 			</p>
 		</div>
 	</div>
