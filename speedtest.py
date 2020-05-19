6ef845bc550c88bff877189ca2a8a5a0215c14fa
Chaim Stanton <chaimstanton1@gmail.com>
2020-05-19 23:58:37 +0100

    get rid of serverity message stuff

--- speedtest.py
+++ speedtest.py
@@ -7,58 +7,53 @@ from time import sleep, strftime
 import csv
 from os import system
 import subprocess
+import json
 
 logging.basicConfig(filename='logfiles.log', filemode='a',
                     format='%(levelname)s:%(message)s:%(asctime)s', datefmt=r"%d/%m/%Y, %H:%M:%S",
                     level=logging.NOTSET)
 
-severityMessgDICT = {1: "info",
-                     2: "good",
-                     3: "BAD "}
 
 
 def quotes(message):
     return "\'" + message + "\'"
 
 class SpeedLog():
-    def __init__(self, message, severity, isConnected=None, output=None):
-        self.message = message
-        self.severity = severity
+    def __init__(self, isConnected, output):
         self.isConnected = isConnected
         self.output=output
 
         self.time = timeSTR()
-        self.severityMSG = severityMessgDICT[severity]
-        self.text = "---" + self.severityMSG + "--- " + "at --- " + self.time + " --- connected=" + str(self.isConnected) + " --- output: " + str(self.output)
+        self.text = self.time + " --- connected=" + str(self.isConnected) + " --- output: " + str(self.output)
 
-        self.logLogFile()
-        # self.logHTML()
         self.logToCSVtable()
+        self.logToJSON
 
     def logToCSVtable(self):
         with open("data.csv", "a") as file:
             writer = csv.writer(file)
-            writer.writerow([self.severityMSG, self.message, self.time])
+            writer.writerow([str(self.isConnected), str(self.time), str(self.output)])
+    
+    def logToJSON(self):
+        pass
 
-    def logLogFile(self):
-        logging.warning(self.text)
 
 
 def timeSTR():
     a = datetime.now()
     return a.strftime(r"%d/%m/%Y, %H:%M:%S")
 
-logging.info("This is beginning of program")
+logging.debug("This is beginning of program")
 
 while True:
     ping = subprocess.run("ping -c 1 8.8.8.8", stdout=subprocess.PIPE, shell=True)
     if ping.returncode == 0:
         pingPositive = True
-        severityMessg = 2 # good
-        message = "Connected"
     else:
         pingPositive = False
-        severityMessgDICT = 3 # bad
-        message = "Not connected"
 
-logging.info("End of program")
+    SpeedLog(pingPositive, ping)
+    break
+    print("trlalala")
+
+logging.debug("End of program")
