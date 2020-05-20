7a47fd3483f828aff947d908ef8fb557bde8c16a
Chaim Stanton <chaimstanton1@gmail.com>
2020-05-20 20:24:17 +0100

    pep8 fixed

--- speedtest.py
+++ speedtest.py
@@ -1,11 +1,9 @@
 #! python3
 
-import urllib.request
 from datetime import datetime
 import logging
-from time import sleep, strftime
+from time import sleep
 import csv
-import os
 import subprocess
 import json
 
@@ -13,13 +11,15 @@ logging.basicConfig(filename='logfiles.log', filemode='a',
                     format='%(levelname)s:%(message)s:%(asctime)s', datefmt=r"%d/%m/%Y, %H:%M:%S",
                     level=logging.NOTSET)
 
+
 def quotes(message):
     return "\'" + message + "\'"
+
 
 class SpeedLog():
     def __init__(self, isConnected, output):
         self.isConnected = isConnected
-        self.output=output
+        self.output = output
 
         self.time = timeSTR()
         self.text = self.time + " --- connected=" + str(self.isConnected) + " --- output: " + str(self.output)
@@ -31,19 +31,18 @@ class SpeedLog():
         with open("data.csv", "a") as file:
             writer = csv.writer(file)
             writer.writerow([str(self.isConnected), str(self.time), str(self.output)])
-    
+
     def __dict__(self):
         return {
-            "isConnected" : self.isConnected,
-            "time" : self.time,
-            "output" : str(self.output)
+            "isConnected": self.isConnected,
+            "time": self.time,
+            "output": str(self.output)
         }
 
     def logToJSON(self):
         with open("data_output.json", "a") as writeFile:
             json.dump(self.__dict__(), writeFile, indent="\t")
             writeFile.write(",\n")
-
 
 
 def timeSTR():
@@ -68,4 +67,3 @@ except KeyboardInterrupt:
     with open("data_output.json", "a") as writeFile:
         writeFile.write("\n}")
     logging.debug("End of program")
-
